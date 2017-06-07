a890ebddeb92c8972f0775ecfe1fca49957c9268
ejc233 <ejc233@cornell.edu>
2017-06-07 15:43:45 -0400

    wrote first draft of average income/intake/outflow functions

--- login_check.py
+++ login_check.py
@@ -47,7 +47,6 @@ def main():
 def checkname(customerId, firstName, lastName, zipcode):
 	url = 'http://api.reimaginebanking.com/customers/{}?key={}'.format(customerId, apiKey)
 
-
 	# Query For Response
 	response = requests.get(url)
 	
@@ -76,7 +75,6 @@ def checkname(customerId, firstName, lastName, zipcode):
 def getaccounts(customerId):
 	url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId, apiKey)
 
-
 	# Query For Response
 	response = requests.get(url)
 	
@@ -90,10 +88,9 @@ def getaccounts(customerId):
 	return raccounts
 
 
-# get the deposits associated with a specific customer id
+# get the deposits associated with a specific account id
 def getdeposits(accountId):
 	url = 'http://api.reimaginebanking.com/accounts/{}/deposits?key={}'.format(accountId, apiKey)
-
 
 	# Query For Response
 	response = requests.get(url)
@@ -109,9 +106,9 @@ def getdeposits(accountId):
 	return rdeposits
 
 
+# get the withdrawals associated with a specific account id
 def getwithdrawals(accountId):
 	url = 'http://api.reimaginebanking.com/accounts/{}/withdrawals?key={}'.format(accountId, apiKey)
-
 
 	# Query For Response
 	response = requests.get(url)
@@ -125,6 +122,88 @@ def getwithdrawals(accountId):
 		rwithdrawals.append(wtuple)
 	
 	return rwithdrawals
+
+
+# get the average intake per transaction in a certain amount of time
+def getAverageIntakeSingleAccountPastDays(accountId, numDays):
+
+	intake = 0.0
+	count = 0.0
+
+	adeposits = getdeposits(accountId)
+	sorted(adeposits, key=lambda x: x[1], reverse=True)
+	for deposit in adeposits:
+		# if date is less than numDays days before current day
+		if datetime.datetime.now() - timedelta(days=numDays) <= deposit[1]:
+			intake += deposit[3]
+			count += 1
+		elif:
+			break
+
+	return intake / count
+
+
+# get the average outflow per transaction and day of week
+def getAverageOutflowSingleAccountPastDays(accountId, numDays):
+
+	outflow = 0.0
+	count = 0.0
+
+	awithdrawals = getwithdrawals(accountId)
+	sorted(awithdrawals, key=lambda x: x[1], reverse=True)
+	for withdrawal in awithdrawals:
+		# if date is less than numDays days before current day
+		if datetime.datetime.now() - timedelta(days=numDays) <= withdrawal[1]:
+			outflow += withdrawal[3]
+			count += 1
+		elif:
+			break
+	
+	return outflow / count
+
+
+# get the average of total income for weeks, hours, days
+def getAverageIncomeSingleAccountPastDays(accountId, numDays):
+
+	return getAverageIntakeSingleAccountPastDays(accountId, numDays) - getAverageOutflowSingleAccountPastDays(accountId, numDays)
+
+
+# get the average intake per transaction for dayRange/2 days before and after middleDay
+def getAverageIntakeSingleAccountRange(accountId, middleDay, dayRange):
+
+	intake = 0.0
+	count = 0.0
+
+	for deposits in getdeposits(accountId):
+		# if date is less than numDays days before current day
+		if middleDay - dayRange / 2 <= deposit[1] && middleDay + dayRange / 2 >= deposit[1]:
+			intake += deposit[3]
+			count += 1
+	
+	return intake / count
+
+
+# get the average outflow per transaction for dayRange/2 days before and after middleDay
+def getAverageOutflowSingleAccountRange(accountId, middleDay, dayRange):
+
+	outflow = 0.0
+	count = 0.0
+
+	for withdrawal in getwithdrawals(accountId):
+		# if date is less than numDays days before current day
+		if middleDay - dayRange / 2 <= withdrawal[1] && middleDay + dayRange / 2 >= withdrawal[1]:
+			outflow += withdrawal[3]
+			count += 1
+	
+	return outflow / count
+
+
+# get the average intake per transaction for dayRange/2 days before and after middleDay
+def getAverageIncomeSingleAccountRange(accountId, middleDay, numDays):
+
+	return getAverageIntakeSingleAccountRange(accountId, middleDay, numDays) - getAverageOutflowSingleAccountRange(accountId, middleDay, numDays)
+
+
 
 
 if __name__ == '__main__':
