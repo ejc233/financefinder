cd22a557994e1e5e7bc7b9e20f54f9f6849ca924
ejc233 <ejc233@cornell.edu>
2017-06-07 13:59:48 -0400

    get accounts function written

--- login_check.py
+++ login_check.py
@@ -33,9 +33,10 @@ def main():
 	cid = '593812c2a73e4942cdafd74f'
 	first = 'Gerda'
 	last = 'Gottlieb'
-	zipcode = '23381'
+	zipcode = '23881'
 		
 	print checkname(cid, first, last, zipcode)
+	print getaccountids(cid)
 
 def checkname(customerId, firstName, lastName, zipcode):
 	url = 'http://api.reimaginebanking.com/customers/{}?key={}'.format(customerId, apiKey)
@@ -54,15 +55,32 @@ def checkname(customerId, firstName, lastName, zipcode):
 	rlast = rj['last_name']
 	rzip = rj['address']['zip']
 	
-	if rfirst.lower().trim() != firstName.lower().trim():
+	if rfirst.lower().strip() != firstName.lower().strip():
 		return -1, 'Customer Information not correct'
-	elif rlast.lower().trim() != lastName.lower().trim():
+	elif rlast.lower().strip() != lastName.lower().strip():
 		return -1, 'Customer Information not correct'
-	elif str(rzip).lower().trim() != str(zipcode).lower().trim():
+	elif rzip != zipcode:
 		return -1, 'Customer Information not correct'
 	
 	
 	return 0, 'Customer information is correct'
+
+
+def getaccountids(customerId):
+	url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId, apiKey)
+
+
+	# Query For Response
+	response = requests.get(url)
+	
+	rj = response.json()
+
+	raccounts = []
+	for account in rj:
+		atuple = (account['_id'], account['nickname'], account['rewards'], account['type'], account['balance'])
+		raccounts.append(atuple)
+	
+	return raccounts
 		
 
 if __name__ == '__main__':
