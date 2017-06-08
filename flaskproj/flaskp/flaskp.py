b50e3d08d139affbb3b53c0f491207fb495cea3d
Robert Marshall <robert.a.marshall@vanderbilt.edu>
2017-06-07 21:30:28 -0400

    Some Flask fixes

--- flaskproj/flaskp/flaskp.py
+++ flaskproj/flaskp/flaskp.py
@@ -4,25 +4,25 @@ app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file
 app.config.from_envvar('FLASKHACK_SETTINGS', silent=True)
 @app.route('/login', methods = ['GET', 'POST'])
+def login():
 	if request.method == 'POST':
 		first = request.form['first']
 		num = request.form['aid']
 		last = request.form['last']
-		zip = request.form['zip']
-		if checkname(num,first,last,zip)[0]==1:
+		zipp = request.form['zip']
+		if checkname(num,first,last,zipp)[0]==1:
 			session['first'] = first
 			session['num'] = num
 			session['last'] = last
-			session['zip'] = zip
-			return redirect(url_for('landing')
+			session['zip'] = zipp
+			return redirect(url_for('landing'))
 		else:
-			return(login.html, error = checkname(num,first,last,zip)[1])
+			error = checkname(num,first,last,zipp)[1]
+			return render_template('login.html', error=error)
 
 @app.route('/landing')
 def show_landing():
-	checking = filter(lambda x: x = 'checking', getaccounts(session['num'])
-	saving=filter(lambda x: x = 'saving', getaccounts(session['num'])
-	credit=filter(lambda x: x = 'credit', getaccounts(session['num'])
+	checking = filter(lambda x: x == 'checking', getaccounts(session['num']))
+	saving=filter(lambda x: x == 'saving', getaccounts(session['num']))
+	credit=filter(lambda x: x == 'credit', getaccounts(session['num']))
 	return render_template('landing.html', checking = checking, saving = saving, credit = credit)
-
-			
