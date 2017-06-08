eba85a88a5531e3ccde035cd465ef587a2ce693b
Benjamin Nichols <benjamin.nichols@capitalone.com>
2017-06-08 09:18:54 -0400

    updating tables to provide method descriptions

--- schema.sql
+++ schema.sql
@@ -1,6 +1,10 @@
-drop table if exists entries;
-create table entries (
-  id integer primary key autoincrement,
-  title text not null,
-  'text' text not null
+drop table if exists actions;
+create table actions (
+  action text primary key ,
+  paramNums integer not null
+);
+create table params(
+	actionName text not null,
+	paramName text not null,
+	foreign key (actionName) references actions(action)
 );
